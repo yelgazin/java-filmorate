d813a00e865247c0c984cb49c5bd9a99c7a7f7ba
Maxim Yelgazin <yelgazin@yandex.ru>
2023-10-01 15:26:43 +0300

    feat: удалить пробел в соответствии со стилем кодирования

--- src/main/java/ru/yandex/practicum/filmorate/validation/DateAfterOrEqual.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/DateAfterOrEqual.java
@@ -6,7 +6,7 @@ import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = DateAfterOrEqualValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target({ ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DateAfterOrEqual {
     String message() default "Неверная дата";
